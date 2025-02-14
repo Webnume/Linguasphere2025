95290a3e6671c2088df0915e27dcac464dc867e7
Yves G. <contact@webnumerik.fr>
2025-02-14 20:54:27 +0100

    fix:

--- app/api/route.ts
+++ app/api/route.ts
@@ -6,27 +6,27 @@
 
 // import { serialize } from "cookie";
 
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
 
-export async function POST(request: Request, params: { slug: string }) {
-  const data: { password: string } = await request.json();
-  const password = data.password;
-  const cookieName = process.env.PASSWORD_COOKIE_NAME!;
-  const pagePassword = process.env.PAGE_PASSWORD!;
+// export async function POST(request: Request, params: { slug: string }) {
+//   const data: { password: string } = await request.json();
+//   const password = data.password;
+//   const cookieName = process.env.PASSWORD_COOKIE_NAME!;
+//   const pagePassword = process.env.PAGE_PASSWORD!;
 
-  if (pagePassword !== password) {
-    console.log(pagePassword);
-    return new Response("incorrect password", {
-      status: 401,
-    });
-  }
-  // Set the cookie using the cookies utility
-  cookies().set(cookieName, "true", {
-    httpOnly: true,
-    path: "/",
-  });
+//   if (pagePassword !== password) {
+//     console.log(pagePassword);
+//     return new Response("incorrect password", {
+//       status: 401,
+//     });
+//   }
+//   // Set the cookie using the cookies utility
+//   cookies().set(cookieName, "true", {
+//     httpOnly: true,
+//     path: "/",
+//   });
 
-  return new Response("password correct", {
-    status: 200,
-  });
-}
+//   return new Response("password correct", {
+//     status: 200,
+//   });
+// }
