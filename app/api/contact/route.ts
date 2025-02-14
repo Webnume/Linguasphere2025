b77d0c05a09ba0f1d157dd4bc05b7574cef87fa5
Yves G. <contact@webnumerik.fr>
2025-02-14 05:25:10 +0100

    fix:

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -7,12 +7,7 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
   );
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
-  if (req.method !== 'POST') {
-    return res.status(405).json({ status: "Method Not Allowed" });
-  }
-
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
   const { name, email, message } = req.body;
 
   if (!name || !email || !message) {
